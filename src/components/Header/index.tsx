5d62791aa7822084d9dd971dbf79863f70fbcd1e
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2022-07-18 10:17:35 -0300

    adicionar alt no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 export function Header(): JSX.Element {
   return (
     <div className={styles.header}>
-      <img src="/images/Logo.svg" alt="spacetraveling" />
+      <Link href="/">
+        <a>
+          <img src="/images/Logo.svg" alt="logo" />
+        </a>
+      </Link>
     </div>
   );
 }
